ac5109b08c641f77b55a54b96faf11ae98134cc9
Mohamed Ahmed <50495610+MohamedAhmed412000@users.noreply.github.com>
2023-08-07 15:24:05 +0300

    Update settings.py

--- BasicBank/settings.py
+++ BasicBank/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['127.0.0.1','.localhost', 'basic-bank-system-mohamedahmed412000.vercel.app', ]
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
