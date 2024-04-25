75773ee314b391c69b8de5a0008b3b7f95b9c29d
Mohamed Ahmed <memoahmed412000@gmail.com>
2024-04-25 14:45:42 +0200

    modify db configuration

--- installing.sh
+++ installing.sh
@@ -1,7 +1,4 @@
-python3 --version
-python3 -m venv env
-source env\scripts\activate
-python3 -m pip install -r requirements_local.txt
-python3 manage.py makemigrations
-python3 manage.py migrate
-python3 manage.py runserver
+python3.9 -m pip install -r requirements_local.txt
+python3.9 manage.py makemigrations
+python3.9 manage.py migrate
+python3.9 manage.py runserver
