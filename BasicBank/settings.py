7196011c55be23be60b9bbfd390f67cd8ca70d9d
Mohamed Ahmed <memoahmed412000@gmail.com>
2024-04-26 20:22:54 +0300

    update database configurations

--- BasicBank/settings.py
+++ BasicBank/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'BasicBank.wsgi.application'
 # Parse the DATABASE_URL to extract the connection details
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASS'),
