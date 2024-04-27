2974521a6b85287c697998621b8d2405a7552f46
Mohamed Ahmed <memoahmed412000@gmail.com>
2024-04-27 11:53:16 +0300

    update settings

--- BasicBank/settings.py
+++ BasicBank/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-from decouple import  config
+from decouple import config
 from pathlib import Path
 import os
 
@@ -129,6 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
