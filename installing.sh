bf2e65d9f3805323c11454a993b07a9674deedc3
Mohamed Ahmed <memoahmed412000@gmail.com>
2024-04-27 12:13:39 +0300

    fix

--- installing.sh
+++ installing.sh
@@ -4,5 +4,6 @@ ls ../../bin
 echo "finished second ls"
 python3.9 -m venv env
 source env/bin/activate
+python3.9 -m pip install --upgrade pip
 python3.9 -m pip install -r requirements_local.txt
 python3.9 manage.py collectstatic --noinput
