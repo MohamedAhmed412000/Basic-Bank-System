3df895c2cdd314f401c9f51c551e9d59ed244256
Mohamed Ahmed <memoahmed412000@gmail.com>
2024-04-27 12:26:11 +0300

    try

--- installing.sh
+++ installing.sh
@@ -1,7 +1,7 @@
-yum install python3-devel
-echo "finished installing python3-dev"
 python3.9 -m venv env
 source env/bin/activate
 python3.9 -m pip install --upgrade pip
+dnf install python3-devel
+echo "finished installing python3-dev"
 python3.9 -m pip install -r requirements_local.txt
 python3.9 manage.py collectstatic --noinput
